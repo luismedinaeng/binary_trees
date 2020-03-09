a9fa91021e2313bf68fa475c7b99551471fb93c5
Jose David Caro <j.caro0207@gmail.com>
2020-03-09 16:50:06 -0500

    [Fix betty task 1, add task 4]

--- binary_trees.h
+++ binary_trees.h
@@ -30,6 +30,7 @@ typedef struct binary_tree_s heap_t;
 /* Binary trees functions */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
 
