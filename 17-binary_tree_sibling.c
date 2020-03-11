c00c90ff4ec67ea1a35c48e69884540a587ae2d9
luismedinaeng <malf95@hotmail.com>
2020-03-11 12:35:44 -0500

    [Style]Fix betty style for task 17

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -1,10 +1,10 @@
 #include "binary_trees.h"
 
 /**
- *binary_tree_sibling - find the sibling of a node
- *node: node to find the sibling
+ * binary_tree_sibling - find the sibling of a node
+ * @node: node to find the sibling
  *
- *Return: sibling of the node
+ * Return: sibling of the node
  */
 binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 {
@@ -12,5 +12,5 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 		return (NULL);
 	if (node->parent->left != node)
 		return (node->parent->left);
-	return(node->parent->right);
+	return (node->parent->right);
 }
