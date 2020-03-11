f4afa1f51c159b10de4b291def8a013966057267
Jose David Caro <j.caro0207@gmail.com>
2020-03-11 13:22:22 -0500

    [Fix task 1] If parent is null the program doesn't create the new node

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,12 +11,17 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *left_node;
 
-	left_node = malloc(sizeof(binary_tree_t));
-	if (parent == NULL || left_node == NULL)
+	if (parent == NULL)
 		return (NULL);
+
+	left_node = malloc(sizeof(binary_tree_t));
+	if (left_node == NULL)
+		return (NULL);
+
 	left_node->n = value;
 	left_node->right = NULL;
 	left_node->parent = parent;
+
 	if (parent->left != NULL)
 	{
 		left_node->left = parent->left;
